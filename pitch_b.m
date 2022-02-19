96b207568b119ea0782602d3a5dcc2b741c87159
XrysaPs <98565363+XrysaPs@users.noreply.github.com>
2022-02-19 14:22:00 +0200

    Add files via upload

---
+++ pitch_b.m
@@ -0,0 +1,27 @@
+function [N, b_coded] = pitch_b(frame,j,previous_frame)
+
+correlation_0=-Inf;
+N=0;
+x = frame(j*40+1:j*40+40);
+
+frame=[previous_frame frame];
+j=j+4;
+
+for lamda=40:1:120
+    y = frame(j*40+1-lamda:j*40+40-lamda);
+    cor = xcorr(x,y);
+    
+    correlation = max(cor);
+    if correlation>correlation_0
+        correlation_0=correlation;
+        N=lamda;
+    end
+end
+
+y = frame(j*40+1-lamda:j*40+40-N);
+
+b=max(xcorr(x,y))/max(xcorr(y));
+
+b_coded= coded_N_b(b);
+
+end
