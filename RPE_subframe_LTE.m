bdfa3efe58bac06d40794067d903cd7253d516a9
XrysaPs <98565363+XrysaPs@users.noreply.github.com>
2022-03-05 21:09:18 +0200

    Update RPE_subframe_LTE.m

--- RPE_subframe_LTE.m
+++ RPE_subframe_LTE.m
@@ -1,18 +1,11 @@
 function [N,b] = RPE_subframe_LTE(d, Prevd)
 
-
 correlation_0 = -Inf;
 N = 0;
 
-% concatenated_frames = [Prevd; d];
-j=3;
-
 for lamda=40:120
-%     y = concatenated_frames(j*40+1-lamda:j*40+40-lamda)
-    y = Prevd(j*40+1-lamda:j*40+40-lamda);
-    cor = xcorr(d,y);
-    
-    
+    y = Prevd(3*40+1-lamda:3*40+40-lamda);
+    cor = xcorr(d,y);    
     correlation = sum(cor);
     if correlation > correlation_0
         correlation_0 = correlation;
@@ -20,10 +13,7 @@ for lamda=40:120
     end
 end
 
-% y = concatenated_frames(j*40+1-N : j*40+40-N)
-y = Prevd(j*40+1-N : j*40+40-N)
-
+y = Prevd(3*40+1-N : 3*40+40-N);
 b = sum(d.*y) ./ sum(y.^2);
-
 
 end
