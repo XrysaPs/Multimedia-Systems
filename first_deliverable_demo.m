c86617ca4b53754de11194df4a35fd525bb93913
AndreasAgg <75082113+AndreasAgg@users.noreply.github.com>
2022-03-06 18:05:28 +0200

    Add files via upload

---
+++ first_deliverable_demo.m
@@ -0,0 +1,24 @@
+clear; clc; close all
+
+[sig,Fs] = audioread("non_periodic.wav","native");
+sig = double(sig);
+frame_size = 160;
+
+
+s=[];
+PrevFrmResd = [];
+for i=1:frame_size:length(sig)
+    if i+frame_size-1 > length(sig)
+        s0 = sig(i:end); 
+    else
+        s0 = sig(i:i+frame_size-1);
+    end
+
+    [LARc, CurrFrmSTResd] = RPE_frame_ST_coder(s0);
+    
+    s0 = RPE_frame_ST_decoder(LARc, CurrFrmSTResd);
+    s = [s; s0];
+
+
+end
+soundsc(s,Fs);
